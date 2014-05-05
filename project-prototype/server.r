b3eb4aef39321109dbbd1c70f438e2b01a3d9706
ashleycoxley <ashleycoxley@gmail.com>
2014-05-04 20:28:28 -0700

    Updating shiny app.

--- project-prototype/server.r
+++ project-prototype/server.r
@@ -21,17 +21,34 @@ getData <- function() {
   # order words by overall frequency
   ordered <- freqs[order(freqs$overall, decreasing=TRUE), ]
   ordered$word <- factor(ordered$word, levels = ordered$word)
+  
+  return(ordered)
+}
+
+getFreq <- function() {
+  file <- 'posting_freqs.csv'
+  post_freq <- read.csv(file, header=T)
+  
+  return(post_freq)
 }
 
 plotFreq <- function(localFrame, showRegion) {
-  if (length(showRegion == 0)){
-    showRegion <- overall
+  if (length(showRegion) == 0){
+    localFrame$showRegion <- localFrame$overall
   }
   
-  localFrame$showRegion <- localFrame[, c(showRegion)]
+  else if (length(showRegion) == 1){
+    localFrame$showRegion <- localFrame[,showRegion]
+  }
   
-  barPlot <- ggplot(subset(localFrame, overall > 10 & word != "NA"), aes(x=word, y=showRegion)) +
+  else {
+    localFrame$showRegion <- rowSums(localFrame[,c(showRegion)])
+  }
+  
+  barPlot <- ggplot(subset(localFrame, overall > 10 & word != "NA"), 
+                    aes(x=word, y=showRegion)) +
     geom_bar(stat='identity') +
+    scale_y_continuous(limits = c(0, 74)) +
     theme(axis.text.x = element_text(angle=35, hjust=1, vjust=1)) +
     theme(axis.title.x = element_blank()) +
     theme(axis.title.y = element_blank()) +
@@ -40,21 +57,54 @@ plotFreq <- function(localFrame, showRegion) {
   return(barPlot)
 }
 
+plotBubble <- function(freqTable) {
+  
+  bubble <- ggplot(freqTable, aes(
+    x = lens,
+    y = normed,
+    color = region,
+    size = pops)) +
+    geom_point() +
+    scale_x_continuous(limits = c(130, 190)) +
+    scale_y_continuous(limits = c(1.5, 4)) +
+    geom_text(aes(label=labels,
+                  hjust = 1,
+                  vjust = .8),
+              color = "grey40",
+              size = 4) +
+    guides(size=FALSE) +
+    theme(legend.position="none") +
+    scale_size_area(max_size=14) +
+    xlab('Posts per 10K Population') +
+    ylab('Median Posting Length (Characters)') +
+    theme(axis.ticks.x = element_blank()) +
+    theme(axis.ticks.y = element_blank())
+    
+  return(bubble)
+}
+
 globalData <- getData()
+freq <- getFreq()
 
 shinyServer(function(input, output) {
   
   cat("Press \"ESC\" to exit...\n")
   
   localFrame <- globalData
+  freqData <- freq
   
-  output$scatterPlot <- renderPlot({
-  barPlot <- barPlot(
+  output$barPlot <- renderPlot({
+  barPlot <- plotFreq(
     localFrame,
-    input$showRegion,
+    input$showRegion
   )
   print(barPlot)
-}
-  )
-}
-)
+  })
+  
+  output$bubblePlot <- renderPlot({
+    bubblePlot <- plotBubble(
+      freqData
+      )
+    print(bubblePlot)
+  })
+})
--- project-prototype/ui.r
+++ project-prototype/ui.r
@@ -1,19 +1,36 @@
 library(shiny)
 
 shinyUI(
-  pageWithSidebar(
-    headerPanel("Craigslist Free in the Bay Area"),
+  pageWithSidebar(    
+    headerPanel('Craigslist Free in the Bay Area'),
     sidebarPanel(
-      checkboxGroupInput(
-        inputId = "showRegion", 
-        label = "Region", 
-        choices = c('San Francisco' = 'sfc', 'Peninsula' = 'pen', 'East Bay' = 'eby', 'North Bay' = 'nby', 'South Bay' = 'sby')
-      )
+    conditionalPanel(condition = 'input.conditionedPanels == 1',
+                     sidebarPanel(
+                       checkboxGroupInput(
+                         inputId = 'showRegion', 
+                         label = "Region", 
+                         choices = c('San Francisco' = 'sfc', 'Peninsula' = 'pen', 'East Bay' = 'eby', 'North Bay' = 'nby', 'South Bay' = 'sby')
+                         )
+                     )
+                     ),
+    
+    conditionalPanel(condition = 'input.conditionedPanels == 2',
+                     sidebarPanel()
+                     )
     ),
     mainPanel(
       tabsetPanel(
-        plotOutput("barPlot")
+        tabPanel("Items",
+                 value = 1,
+                 plotOutput('barPlot')
+                 ),
+        tabPanel("Posting Habits",
+                 value = 2,
+                 plotOutput('bubblePlot')
+                 ),
+        id = "conditionedPanels"
       )
     )
   )
 )
+
